4296249c6f66b167faf782510797a98a54935862
cardanians <58650764+cardanians@users.noreply.github.com>
2020-01-18 13:04:34 +0100

    Update example_config_generate.sh

--- example_config_generate.sh
+++ example_config_generate.sh
@@ -7,7 +7,7 @@
 # curl -d "api_key=KEY&storage=STORAGE&cpu=NUM_OF_CPUS" -X POST https://api.adapools.org/v0/config/ -o /where/is/located/config.yaml
 #
 # possible include this into "anti-stucking" script
-3
+#
 
 curl -d "api_key=ABCABCABC&storage=/root/storage&cpu=4" -X POST https://api.adapools.org/v0/config/ -o /root/config.yaml
 
